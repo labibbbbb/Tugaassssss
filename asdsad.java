d669330e358d9edb98764ad36994fa228fc785f4
labibbbbb <163399145+labibbbbb@users.noreply.github.com>
2024-03-14 16:04:13 +0700

    Create asdsad.java

---
+++ asdsad.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.latihan140324;
+
+/**
+ *
+ * @author LAB
+ */
+public class Latihan140324 {
+
+    public static void main(String[] args) {
+        
+        
+        String nama = "Labib Wibisono";
+        int umur = 19;
+        String npm = "2311010067";
+        boolean hujan = false;
+        int x = 8;
+        int y = 4;
+                
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(npm);
+        
+        ///System.out.println("Nama = "+nama);
+       ///System.out.println("Umur = "+umur);
+       ///System.out.println("NPM = "+npm);
+       if (hujan) {
+           System.out.println("Keluar Rumah");
+       }
+        else
+           System.out.println("Tidak Keluar Rumah");
+       }
+       System.out.println(x + y);
+    }
+}
